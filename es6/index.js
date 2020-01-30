d8a00b28f360bab9c58021c07d9c1a2b1263a10a
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-01-30 11:30:01 -0500

    Clases, Módulos y Generadores

--- es6/index.js
+++ es6/index.js
@@ -133,3 +133,39 @@ const helloPromise = () => {
 helloPromise()
   .then(response => console.log(response))
   .catch(error => console.log(error));
+
+/** Clases ES6 */
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+
+  sumar(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sumar(2, 2));
+
+import { hello } from "./module";
+
+hello();
+
+/** Generator ES6 */
+function* helloWorld() {
+  if (true) {
+    yield "Hello, ";
+  }
+  if (true) {
+    yield "World";
+  }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
