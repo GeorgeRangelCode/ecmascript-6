c0c004bf731829fd9b8c5aab49524735391c9aab
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-01-30 12:00:29 -0500

    Async y Await ES8

--- es8/index.js
+++ es8/index.js
@@ -21,3 +21,30 @@ console.log(values);
 const string = "hello";
 console.log(string.padStart(8, "hi "));
 console.log(string.padEnd(10, " Juan"));
+
+/** Async await */
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Hello world"), 3000)
+      : reject("No funciono");
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
