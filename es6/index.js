e8cd5426727d49037183a080524e35e21f6ad41a
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-01-30 10:31:10 -0500

    LET y CONST, Multilinea, Spread Operator y Desestructuración

--- es6/index.js
+++ es6/index.js
@@ -1,4 +1,5 @@
 /** ES5 */
+let a = "";
 function newFuntion(name, age, country) {
   var name = name || "Jorge";
   var age = age || 25;
@@ -24,3 +25,48 @@ console.log(hi);
 /** ES6 Template literals */
 let hi2 = `${hello} ${world}`;
 console.log(hi2);
+
+let lorem = "lorem dg shgdf tsdf sdfsd hsdgfs gf \n" + "dghfsd dsfhusdf hjdf.";
+
+let lorem2 = `lorem dg shgdf tsdf sdfsd hsdgfs gf
+dghfsd dsfhusdf hjdf.`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  name: "Jorge",
+  age: 25,
+  country: "Colombia"
+};
+
+console.log(person.name, person.age, person.country);
+
+/** Desestructuración */
+
+let { name, age, country } = person;
+console.log(name, age, country);
+
+let { name } = person;
+console.log(name);
+
+/** Operador de propagación */
+let team1 = ["pedro", "pablo", "jorge"];
+let team2 = ["valeria", "jessica", "juana"];
+
+let education = ["david", ...team1, ...team2];
+console.log(education);
+
+{
+  var globalVar = "global Var";
+}
+
+{
+  let globalLet = "global Let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = "b";
+console.log(a);
